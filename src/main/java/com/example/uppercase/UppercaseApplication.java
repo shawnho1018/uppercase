8a1d57f7a3eb423c7d1f84a465930fdd52508757
Shawn Ho <hos@hos-a01.vmware.com>
2019-01-05 11:13:00 +0800

    first commit

---
+++ src/main/java/com/example/uppercase/UppercaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.uppercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppercaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppercaseApplication.class, args);
+	}
+
+}
+
